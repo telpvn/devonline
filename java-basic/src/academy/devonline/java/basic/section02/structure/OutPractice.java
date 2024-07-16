84d2a1be32b3011d51a10ff1a4effb395182575f
telpvn <telpvn@mail.ru>
2024-07-16 15:37:26 +0400

    Out practice

---
+++ java-basic/src/academy/devonline/java/basic/section02/structure/OutPractice.java
@@ -0,0 +1,21 @@
+package academy.devonline.java.basic.section02.structure;
+
+/**
+ * Создать программу OutPractice, которая выводит на консоль все числа от 0 до 9 включительно, которые нацело делятся на 3, т.е. 0, 3, 6 и 9 сначала в строку, а затем каждое число с новой строчки.
+  */
+
+public class OutPractice {
+    public static void main(String[] args) {
+        System.out.print(0);
+        System.out.print(3);
+        System.out.print(6);
+        System.out.print(9);
+
+        System.out.println();
+
+        System.out.println(0);
+        System.out.println(3);
+        System.out.println(6);
+        System.out.println(9);
+    }
+}

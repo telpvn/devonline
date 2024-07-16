1186489b921c279507cfaa896b7d34f53636c7bb
telpvn <telpvn@mail.ru>
2024-07-16 18:51:58 +0400

    In 2

--- java-basic/src/academy/devonline/java/basic/section02/structure/In.java
+++ java-basic/src/academy/devonline/java/basic/section02/structure/In.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 public class In {
     public static void main(String[] args) {
         var a = new Scanner(System.in).nextInt();
-        var b = new Scanner(System.in).nextInt();
+        System.out.println(a);
 
-        System.out.println(a + b);
+        var d = new Scanner(System.in).nextDouble();
+        System.out.println(d);
+
+        var b = new Scanner(System.in).nextBoolean();
+        System.out.println(b);
+
+        var ch = new Scanner(System.in).nextLine().charAt(0);
+        System.out.println(ch);
+
+        var s = new Scanner(System.in).nextLine();
+        System.out.println(s);
     }
 }

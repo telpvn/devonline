13b74a997cfbb7c04e83cc8f689c2e2d07b7e590
telpvn <telpvn@mail.ru>
2024-07-16 17:55:41 +0400

    VarVar definition

---
+++ java-basic/src/academy/devonline/java/basic/section02/structure/VarDefinition.java
@@ -0,0 +1,11 @@
+package academy.devonline.java.basic.section02.structure;
+
+public class VarDefinition {
+    public static void main(String[] args) {
+        var i = 1;
+        System.out.println(i);
+
+        var j = 1;
+        System.out.println(j);
+    }
+}

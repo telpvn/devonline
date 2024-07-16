e10318c2da9229179bc1959ee9f96548ed099907
telpvn <telpvn@mail.ru>
2024-07-16 18:01:57 +0400

    Comments

---
+++ java-basic/src/academy/devonline/java/basic/section02/structure/Comments.java
@@ -0,0 +1,20 @@
+package academy.devonline.java.basic.section02.structure;
+
+public class Comments {
+    public static void main(String[] args) {
+        // alkdfjadkfas;fjafkjads;fklj
+
+        /*
+        jfasd;lkfja;dklfja;fdj
+        fasdjf;lakjf;lasj
+
+        dajfla;kfj;askdj;a
+         */
+
+        /**
+         * dasfkj;ksd
+         * flakjsfla;jk
+         * adljskf;df
+         */
+    }
+}

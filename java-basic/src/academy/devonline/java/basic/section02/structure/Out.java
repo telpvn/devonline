fe61db920c5a6672ee943af6376c7c07c4245920
telpvn <telpvn@mail.ru>
2024-07-16 15:19:15 +0400

    Out

---
+++ java-basic/src/academy/devonline/java/basic/section02/structure/Out.java
@@ -0,0 +1,20 @@
+package academy.devonline.java.basic.section02.structure;
+
+public class Out {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(1);
+        System.out.println(1.2);
+        System.out.println(1 + 4);
+
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+
+        System.out.print(1);
+        System.out.print(2);
+        System.out.print(3);
+        System.out.print(4);
+    }
+}

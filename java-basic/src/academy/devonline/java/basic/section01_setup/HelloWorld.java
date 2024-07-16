ba40d30c04dd9b2971cb5ec56eaed03a6ca8f1b3
telpvn <telpvn@mail.ru>
2024-07-16 14:41:29 +0400

    Hello world

---
+++ java-basic/src/academy/devonline/java/basic/section01_setup/HelloWorld.java
@@ -0,0 +1,7 @@
+package academy.devonline.java.basic.section01_setup;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ java-basic/src/academy/devonline/java/basic/section01_setup/ILoveJava.java
@@ -0,0 +1,7 @@
+package academy.devonline.java.basic.section01_setup;
+
+public class ILoveJava {
+    public static void main(String[] args) {
+        System.out.println("I love Java");
+    }
+}

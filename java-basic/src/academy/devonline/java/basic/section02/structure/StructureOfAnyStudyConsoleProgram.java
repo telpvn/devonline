ef588a9ca8f52d00e202a92712c2dc39a67cbb57
telpvn <telpvn@mail.ru>
2024-07-17 17:03:03 +0400

    Using constant values instead of reading from the console

---
+++ java-basic/src/academy/devonline/java/basic/section02/structure/StructureOfAnyStudyConsoleProgram.java
@@ -0,0 +1,12 @@
+package academy.devonline.java.basic.section02.structure;
+
+public class StructureOfAnyStudyConsoleProgram {
+    public static void main(String[] args) {
+        var a = 8;//new Scanner(System.in).nextInt();
+        var b = 9;//new Scanner(System.in).nextInt();
+
+        var result = a + b * ((5 - 7) * a);
+
+        System.out.println(result);
+    }
+}

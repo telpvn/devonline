a774d0c45587c8e48040a2a255d300cf0bdf5e5c
telpvn <telpvn@mail.ru>
2024-07-17 17:08:52 +0400

    Practice_Changing the greeting program

---
+++ java-basic/src/academy/devonline/java/basic/section02/structure/HelloStudyUser.java
@@ -0,0 +1,9 @@
+package academy.devonline.java.basic.section02.structure;
+
+public class HelloStudyUser {
+    public static void main(String[] args) {
+        var name = "Valera";
+
+        System.out.println("Hello, " + name);
+    }
+}

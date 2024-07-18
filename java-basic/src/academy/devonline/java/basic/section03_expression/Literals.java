cf08ec1335f3abc14762bb808ddc94e9e87483b2
telpvn <telpvn@mail.ru>
2024-07-18 13:29:06 +0400

    Literals

---
+++ java-basic/src/academy/devonline/java/basic/section03_expression/Literals.java
@@ -0,0 +1,23 @@
+package academy.devonline.java.basic.section03_expression;
+
+public class Literals {
+    public static void main(String[] args) {
+        // int
+        var i = 1234;
+        // double
+        var d1 = 1.;
+        var d2 = 1.0;
+        var d3 = 1.123456;
+        var d4 = 5e3; // 5 * 10 ^ 3 = 5000.0
+        // boolean
+        boolean b1 = true;
+        boolean b2 = false;
+        // char
+        var ch1 = 'a';
+        var ch2 = '\t';
+        var ch3 = '\n';
+        // String
+        var s1 = "a";
+        var s2 = "Hello world";
+    }
+}

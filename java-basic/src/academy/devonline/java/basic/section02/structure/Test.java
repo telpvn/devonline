f5cd351199714243143cb891d706fa87f25bf94c
telpvn <telpvn@mail.ru>
2024-07-20 11:50:18 +0400

    Simple calculator 3

---
+++ java-basic/src/academy/devonline/java/basic/section02/structure/Test.java
@@ -0,0 +1,14 @@
+package academy.devonline.java.basic.section02.structure;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        new Scanner(System.in).nextBoolean();
+        new Scanner(System.in).nextInt();
+        new Scanner(System.in).nextDouble();
+        var c = new Scanner(System.in).nextLine().charAt(0);
+        var s = new Scanner(System.in).nextLine();
+
+    }
+}

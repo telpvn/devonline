81b9535d3bb63648f4a3bac08e5b7299ae4bc609
telpvn <telpvn@mail.ru>
2024-07-20 07:56:06 +0400

    Arithmetic expressions

---
+++ java-basic/src/academy/devonline/java/basic/section03_expression/ArithmeticExpressions.java
@@ -0,0 +1,27 @@
+package academy.devonline.java.basic.section03_expression;
+
+public class ArithmeticExpressions {
+    public static void main(String[] args) {
+        {
+            var a = 5;
+            var b = 3;
+
+            System.out.println(a + b);
+            System.out.println(a - b);
+            System.out.println(a * b);
+            System.out.println(a / b);
+            System.out.println(a % b);
+        }
+        System.out.println();
+        {
+            var a = 5.;
+            var b = 3.;
+
+            System.out.println(a + b);
+            System.out.println(a - b);
+            System.out.println(a * b);
+            System.out.println(a / b);
+            System.out.println(a % b);
+        }
+    }
+}

e20cc4d0f9960cf6ef37c14ffb423d53d7842e6c
telpvn <telpvn@mail.ru>
2024-07-20 11:47:16 +0400

    Simple calculator 2

--- java-basic/src/academy/devonline/java/basic/section03_expression/SimpleCalculator.java
+++ java-basic/src/academy/devonline/java/basic/section03_expression/SimpleCalculator.java
@@ -4,17 +4,20 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
     public static void main(String[] args) {
+        // read source data
         System.out.println("Введите первое число: ");
         var a = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число: ");
         var b = new Scanner(System.in).nextInt();
 
+        // processing
         var aPlusB = a + b;
         var aMinusB = a - b;
         var aMulB = a * b;
         var aDivB = a / b;
         var aModB = a % b;
 
+        // display results
         System.out.println("a + b = " + aPlusB);
         System.out.println("a - b = " + aMinusB);
         System.out.println("a * b = " + aMulB);

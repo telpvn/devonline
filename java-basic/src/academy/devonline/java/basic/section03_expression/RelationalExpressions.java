5c8e90dba96c56d1e4cea1d07ceccc16dda207c6
telpvn <telpvn@mail.ru>
2024-07-20 13:55:31 +0400

    Relational expressions

---
+++ java-basic/src/academy/devonline/java/basic/section03_expression/RelationalExpressions.java
@@ -0,0 +1,24 @@
+package academy.devonline.java.basic.section03_expression;
+
+public class RelationalExpressions {
+    public static void main(String[] args) {
+        var a = 2;
+        var b = 5;
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a < b);
+        System.out.println(a <= b);
+
+        System.out.println(a >= 8);
+        System.out.println(a == 9);
+
+        var c1 = a == b;
+        boolean c2 = a == b;
+
+        System.out.println(c1);
+        System.out.println(c2);
+    }
+}

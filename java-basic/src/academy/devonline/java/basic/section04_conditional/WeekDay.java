d124fa68cfcade8676694b23dab6707b3d0daf6a
telpvn <telpvn@mail.ru>
2024-07-26 19:18:29 +0400

    Week day

---
+++ java-basic/src/academy/devonline/java/basic/section04_conditional/WeekDay.java
@@ -0,0 +1,32 @@
+package academy.devonline.java.basic.section04_conditional;
+
+import java.util.Scanner;
+
+public class WeekDay {
+    public static void main(String[] args) {
+        // read source date
+        System.out.println("Введите номер дня недели:");
+        var day = new Scanner(System.in).nextInt();
+        // processing
+        String result;
+        if (day == 1) {
+            result = "Monday";
+        } else if (day == 2) {
+            result = "Tuesday";
+        } else if (day == 3) {
+            result = "Wednesday";
+        } else if (day == 4) {
+            result = "Thursday";
+        } else if (day == 5) {
+            result = "Friday";
+        } else if (day == 6) {
+            result = "Saturday";
+        } else if (day == 7) {
+            result = "Sunday";
+        } else {
+            result = "Вы ввели не корректное число" + day;
+        }
+        // display result
+        System.out.println(result);
+    }
+}
